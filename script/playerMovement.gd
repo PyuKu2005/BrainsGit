918002aba50ba36ecdafdb5aff0fd812d80e5404
PyuKu2005 <allardl2005@gmail.com>
2024-11-13 22:31:06 -0500

    Changement du sprite du personnage principal, ajout de la fonctionnalité de combo
    Ajout du combo et rêglage de certains bugs mineurs(oui déjà)

--- script/playerMovement.gd
+++ script/playerMovement.gd
@@ -1,61 +1,78 @@
-extends CharacterBody3D 
-
+extends CharacterBody3D
 @export var SPEED = 5.0
 @export var sprintSpeed = 7.0
 @onready var area3d = $Sprite3D/playerHitArea
 @onready var animation = $AnimationPlayer
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var playerHealth = 100
+
 var isAttacking = false
+var attackSpeed = SPEED / 5.0  # Reduced speed during attacks
+var currentAttackAnimation = "Attack2"  # Start with Attack2
+var attackComboTimer = 0.0  # Timer to manage switching between Attack2 and Attack
+var comboDelay = 0.5  # Delay between switching attacks in the loop
 
 ##### DÉBUT PROCESS DELTA #####
 func _physics_process(delta):
+	# Apply gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	############## MOVEMENT ###############
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var currentSpeed = SPEED
 
-	# Define reduced speed for attacking
-	var attackSpeed = SPEED / 5.0
-	var currentSpeed = SPEED  
-	
-	###### COURIR (Sprinting) #######
 	if Input.is_action_pressed("Courir") and direction and not isAttacking:
+		isAttacking == false
 		currentSpeed = sprintSpeed
-	elif isAttacking:
-		currentSpeed = attackSpeed  
+	
+	if isAttacking:
+		currentSpeed = attackSpeed
 
-	# Movement and animation logic
 	if direction:
 		velocity.x = direction.x * currentSpeed
 		velocity.z = direction.z * currentSpeed
-
-		
 		if not isAttacking:
 			animation.play("BrainMoving", -1, 2.0 if currentSpeed == sprintSpeed else 1.0, true)
 
-		if input_dir.x < 0:  
+		if input_dir.x < 0:  # Moving left
 			$Sprite3D.flip_h = true
 			area3d.scale.x = -abs(area3d.scale.x)
-		elif input_dir.x > 0:  
-			$Sprite3D.flip_h = false  
+		elif input_dir.x > 0:  # Moving right
+			$Sprite3D.flip_h = false
 			area3d.scale.x = abs(area3d.scale.x)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		if not isAttacking:
 			animation.play("BrainIdle", -1, 1.0, true)
+
 	move_and_slide()
 
-	###### COMBAT #######
-	if Input.is_action_just_pressed("playerAttack") and not isAttacking:
-		isAttacking = true
-		if animation.has_animation("Attack"):
-			animation.play("Attack")
-	if isAttacking and not Input.is_action_pressed("playerAttack"):
-		isAttacking = false
+	if Input.is_action_pressed("playerAttack"):
+		if not isAttacking:
+			isAttacking = true
+			animation.play(currentAttackAnimation, -1, 1.0, true)  # Start with Attack2 animation
+		else:
+			if attackComboTimer <= 0:
+				if currentAttackAnimation == "Attack2":
+					currentAttackAnimation = "Attack"
+				else:
+					currentAttackAnimation = "Attack2"
+				attackComboTimer = comboDelay 
+			animation.play(currentAttackAnimation, -1, 1.0, true)
+		
+		attackComboTimer -= delta
+
+	if not Input.is_action_pressed("playerAttack"):
+		if isAttacking:
+			isAttacking = false
+			attackComboTimer = 0  
+			currentAttackAnimation = "Attack2"  
+		if direction.length() > 0:
+			animation.play("BrainMoving", -1, 1.0, true)
+		else:
+			animation.play("BrainIdle", -1, 1.0, true)
 
 	############# CAMERA ################
 	var camera_position = $CameraControl.position
@@ -65,7 +82,7 @@ func _physics_process(delta):
 	$CameraControl.position = camera_position
 ##### FIN PROCESS DELTA ##### 
 
-################ RECEVOIR DAMAGES ################
+################ RECEVOIR DAMAGE ################
 func hurt(hitPoints):
 	if hitPoints < playerHealth:
 		playerHealth -= hitPoints
